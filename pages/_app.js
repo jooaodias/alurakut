9ab2b5a02df11628fd7a92baa73f742fc8ef0d55
jooaodias <joaovitordias92040@gmail.com>
2021-07-13 00:07:58 -0300

    imersão react starts

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,37 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { AlurakutStyles } from '../src/lib/alurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+
+  body {
+    background-color: #D9E6F6;
+    font-family: sans-serif;
+  }
+
+  #__next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  ${AlurakutStyles}
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +41,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,68 @@
-import styled from 'styled-components'
+import { Box } from '../src/components/Box';
+import { MainGrid } from '../src/components/MainGrid';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from '../src/lib/alurakutCommons';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        style={{ borderRadius: '8px' }}
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const user = 'jooaodias';
+  const pessoasFavs = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'felipefialho',
+  ];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSideBar githubUser={user} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">Bem vindo(a)</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: 'profileRelationsArea' }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smalltitle">
+              Pessoas da comunidade ({pessoasFavs.length})
+            </h2>
+            <ul>
+              {pessoasFavs.map(pessoa => {
+                return (
+                  <li>
+                    <a href={`/users/${pessoa}`} key={pessoa}>
+                      <img src={`https://github.com/${pessoa}.png`} />
+                      <span>{pessoa}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
